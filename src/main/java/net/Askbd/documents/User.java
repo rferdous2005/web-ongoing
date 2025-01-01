61c2c2dd50295a03ef2437511ec5a8c4a554a422
rferdous2005 <raiyan.csebuet@gmail.com>
2025-01-01 12:16:46 +0600

    Delete src directory

--- src/main/java/net/Askbd/documents/User.java
+++
@@ -1,8 +0,0 @@
-package net.Askbd.documents;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "users")
-public class User {
-    private String firstName, lastName, email, mobile;
-}
