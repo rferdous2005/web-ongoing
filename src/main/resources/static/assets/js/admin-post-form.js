e0e2a779c5c95fedaa1157bb32d7a333360efc1a
Raiyan Ferdous <raiyan.csebuet@gmail.com>
2025-01-23 17:08:06 +0600

    quill editor scroll

--- src/main/resources/static/assets/js/admin-post-form.js
+++ src/main/resources/static/assets/js/admin-post-form.js
@@ -1,17 +1,21 @@
+const IMAGE_WIDTH = "750";
+const IMAGE_HEIGHT = "500";
+
 function activateQuillEditor(elementID) {
   var toolbarOptions = [
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
       ['bold', 'italic', "underline", "strike"],
       [{ 'size': ['small', false, 'large', 'huge'] }],
       [{ 'color': [] }, { 'background': [] }],
-      ['link', 'image'],
+      ['link', 'image', 'code-block'],
       [{ 'align': [] }],
       [{ list:  "ordered" }, { list:  "bullet" }],
       [{'font': []}],
-  ]
+  ];
   var editor = new Quill(elementID, 
   {
       theme: "snow", 
+      scrollingContainer: elementID+"Container",
       modules: {
           toolbar: toolbarOptions
       }
@@ -19,6 +23,7 @@ function activateQuillEditor(elementID) {
   var descriptionHTML;
   descriptionHTML = $(".ql-editor").html();
   console.log(descriptionHTML);
+
   $(elementID).find(".ql-editor").keyup( function(e) {
       descriptionHTML = $(".ql-editor").html();
       $(elementID + "Html").val(descriptionHTML);
@@ -29,22 +34,33 @@ function activateQuillEditor(elementID) {
       $(elementID + "Html").val(descriptionHTML);
       //console.log(descriptionHTML);
   });
+
+  return editor;
 }
-  
+
+function clickedInsertImage() {
+  var inputURL = prompt("Insert Img URL");
+  var htmlImgTag = `<br/><img src="${inputURL}" width="${IMAGE_WIDTH}" height="${IMAGE_HEIGHT}" /><br/>`;
+  range = quill.getSelection();
+  quill.insertText(range.index, htmlImgTag);
+}
+
 function clickedShowHtml() {
     var html = $(".ql-editor").html();
     $(".ql-editor").text(html);
     $("#bodyDescriptionHtml").val(html);
     $("#showHtmlButton").hide();
     $("#showBeautifiedButton").show();
+    $("#showInsertImageButton").show();
     $("#showBeautifiedButton").prop("disabled", false);
-  }
+}
 
 function clickedShowBeautified() {
     var htmlText = $(".ql-editor").text();
     $(".ql-editor").html(htmlText)
     $("#bodyDescriptionHtml").val(htmlText);
     $("#showBeautifiedButton").hide();
+    $("#showInsertImageButton").hide();
     $("#showHtmlButton").show();
   }
 
@@ -56,4 +72,4 @@ function enteredTitleText(inputBox) {
     $("#postUri").val(uri);
   }
   
-  activateQuillEditor("#bodyDescription");
+var quill = activateQuillEditor("#bodyDescription");
