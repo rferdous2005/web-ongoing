a21b2640f0897cff8562ec97159b06151bda2d5b
Raiyan Ferdous <raiyan.csebuet@gmail.com>
2025-06-04 12:25:25 +0600

    Warning label & highlights fetching and saving update done

---
+++ src/main/java/com/cirt/web/entity/Incident.java
@@ -0,0 +1,30 @@
+package com.cirt.web.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Incident {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String reportType;
+    @Column(nullable = true, length = 100)
+    private String name, orgName, contactName, domainIP, fileName;
+    @Column(nullable = false, length = 50)
+    private String email, phone, region, incidentType, affectedAsset, discovery, attackVector, impact, ongoing;
+    @Column(nullable = false, length = 500)
+    private String location, description, stepsTaken;
+
+    private LocalDateTime incidentTime;
+}
